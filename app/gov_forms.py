257b7647ffcf7f7808a9cb3ddff853a9f41d8a06
Matt Shaw <matthew.shaw@landregistry.gov.uk>
2021-02-08 23:56:27 +0000

    submit and error summary

--- app/gov_forms.py
+++ app/gov_forms.py
@@ -1,6 +1,6 @@
 from flask_wtf import FlaskForm
-from govuk_frontend_wtf.wtforms_widgets import GovTextInput
-from wtforms import StringField
+from govuk_frontend_wtf.wtforms_widgets import GovSubmitInput, GovTextInput
+from wtforms import StringField, SubmitField
 from wtforms.validators import InputRequired
 
 
@@ -8,3 +8,4 @@ class ExampleForm(FlaskForm):
     full_name = StringField(
         "Full name", widget=GovTextInput(), validators=[InputRequired(message="Enter your full name")]
     )
+    submit = SubmitField("Continue", widget=GovSubmitInput())
