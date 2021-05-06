a6877ab06d7d5582460711eb66eef69c4c5a0139
Matt Shaw <matthew.shaw@landregistry.gov.uk>
2021-05-06 10:38:21 +0100

    define regex as raw strings

--- app/forms.py
+++ app/forms.py
@@ -42,7 +42,7 @@ class BankDetailsForm(FlaskForm):
         widget=GovTextInput(),
         validators=[
             InputRequired(message="Enter a sort code"),
-            Regexp(regex="\d{6}", message="Enter a valid sort code like 309430"),
+            Regexp(regex=r"\d{6}", message="Enter a valid sort code like 309430"),
         ],
         description="Must be 6 digits long",
     )
@@ -51,7 +51,7 @@ class BankDetailsForm(FlaskForm):
         widget=GovTextInput(),
         validators=[
             InputRequired(message="Enter an account number"),
-            Regexp(regex="\d{6,8}", message="Enter a valid account number like 00733445"),
+            Regexp(regex=r"\d{6,8}", message="Enter a valid account number like 00733445"),
             Length(min=6, max=8, message="Account number must be between 6 and 8 digits"),
         ],
         description="Must be between 6 and 8 digits long",
@@ -67,7 +67,7 @@ class BankDetailsForm(FlaskForm):
                 message="Building society roll number must be between 1 and 18 characters",
             ),
             Regexp(
-                regex="[a-zA-Z0-9- /.]*$",
+                regex=r"[a-zA-Z0-9- /.]*$",
                 message="Building society roll number must only include letters a to z, numbers, hyphens, spaces, forward slashes and full stops",
             ),
         ],
@@ -91,7 +91,7 @@ class CreateAccountForm(FlaskForm):
         "Date of birth",
         widget=GovDateInput(),
         validators=[InputRequired(message="Enter your date of birth")],
-        description="For example, 31 3 1980"
+        description="For example, 31 3 1980",
     )
     national_insurance_number = StringField(
         "National Insurance number",
@@ -103,7 +103,7 @@ class CreateAccountForm(FlaskForm):
                 message="National Insurance number must be 13 characters or fewer",
             ),
             Regexp(
-                regex="^[a-zA-Z]{2}\d{6}[aAbBcCdD]$",
+                regex=r"^[a-zA-Z]{2}\d{6}[aAbBcCdD]$",
                 message="Enter a National Insurance number in the correct format",
             ),
         ],
@@ -125,7 +125,7 @@ class CreateAccountForm(FlaskForm):
         validators=[
             InputRequired(message="Enter a UK telephone number"),
             Regexp(
-                regex="[\d \+]",
+                regex=r"[\d \+]",
                 message="Enter a telephone number, like 01632 960 001, 07700 900 982 or +44 0808 157 0192",
             ),
         ],
